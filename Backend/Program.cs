b3e50f7f9332acb4b99dffc97756fbc0a27f1642
Miguel Lima <mglima1279@gmail.com>
2026-06-05 14:06:05 -0300

    Add cors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -2,11 +2,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
